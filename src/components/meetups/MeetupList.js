2c752f56831ab7e6055e79cbd02b2c435e751660
rina rim <rina.rim803@gmail.com>
2022-11-18 16:33:44 -0800

    update Meetup pages
    - fix typos
    - update files using snippets
    - add components

---
+++ src/components/meetups/MeetupList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './MeetupList.module.css';
+
+function MeetupList() {
+  return (
+    <div>MeetupList</div>
+  )
+}
+
+export default MeetupList;
--- src/pages/Favourites.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function FavouritesPage() {
-  return <div>Favourites Page</div>;
-}
-
-export default FavouritesPage;
