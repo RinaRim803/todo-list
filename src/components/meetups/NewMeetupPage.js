9eff3ace1789a7d4fea6a4a0ceba2076ced3a314
RinaRim803 <rina.rim803@gmail.com>
2022-11-28 19:06:11 -0800

    Add a submission form

--- src/components/meetups/NewMeetupPage.js
+++ src/components/meetups/NewMeetupPage.js
@@ -1,12 +1,25 @@
-import React from 'react'
-import NewMeetupForm from './NewMeetupForm'
+import React from "react";
+import NewMeetupForm from "./NewMeetupForm";
 
 function NewMeetupPage() {
+  function onAddMeetupHandler(meetupData) {
+    fetch(
+      "https://react-getting-started-be2ec-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: 'POST', 
+        body: JSON.stringify(meetupData),
+        headers:{
+            'Content-Type': 'application/json'
+        }
+      }
+    );
+  }
+
   return (
     <div>
-        <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={onAddMeetupHandler} />
     </div>
-  )
+  );
 }
 
-export default NewMeetupPage
+export default NewMeetupPage;
