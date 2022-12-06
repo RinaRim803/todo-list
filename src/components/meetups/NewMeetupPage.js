eab00e8726eeb3018c129923690ad623dca79ec7
RinaRim803 <rina.rim803@gmail.com>
2022-12-05 23:52:51 -0800

    Add Fetching data
    - add fetching data from firebase

--- src/components/meetups/NewMeetupPage.js
+++ src/components/meetups/NewMeetupPage.js
@@ -1,18 +1,24 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
 import NewMeetupForm from "./NewMeetupForm";
 
 function NewMeetupPage() {
+  const navigate = useNavigate();
+
   function onAddMeetupHandler(meetupData) {
     fetch(
       "https://react-getting-started-be2ec-default-rtdb.firebaseio.com/meetups.json",
       {
-        method: 'POST', 
+        method: "POST",
         body: JSON.stringify(meetupData),
-        headers:{
-            'Content-Type': 'application/json'
-        }
+        headers: {
+          "Content-Type": "application/json",
+        },
       }
-    );
+    ).then(() => {
+      navigate("/", { replace: true });
+    });
   }
 
   return (
--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_DATA = [
@@ -9,7 +9,7 @@ const DUMMY_DATA = [
       "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/VancouverIsland.A2003154.1930.250m.cropped.jpg/390px-VancouverIsland.A2003154.1930.250m.cropped.jpg",
     address: "Meetupstreet 5, 12345 Meetup City",
     description:
-      "This is a first, amazing meetup which you definitely should not miss."
+      "This is a first, amazing meetup which you definitely should not miss.",
   },
   {
     id: "m2",
@@ -18,11 +18,44 @@ const DUMMY_DATA = [
       "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Vancouver_de.png/390px-Vancouver_de.png",
     address: "Meetupstreet 5, 12345 Meetup City",
     description:
-      "This is a second, amazing meetup which you definitely should not miss."
-  }
+      "This is a second, amazing meetup which you definitely should not miss.",
+  },
 ];
 
 function AllMeetupsPage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      "https://react-getting-started-be2ec-default-rtdb.firebaseio.com/meetups.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+        for(const key in data) {
+          const meetup={
+            id: key,
+            ...data[key]
+          };
+          meetups.push(meetup);
+        }
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
   return (
     <section>
       <h1>All Meetups</h1>
